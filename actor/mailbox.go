23b2e4e7a5e4646aec105d492a61c772874ce14f
milossdjuric <milossdjuric@yahoo.com>
2024-06-15 19:59:02 +0200

    add skeleton for Mailbox

--- actor/mailbox.go
+++ actor/mailbox.go
@@ -1,4 +1,40 @@
 package actor
 
 type Mailbox struct {
+	actorChan chan Envelope
+	queue     []Envelope
+}
+
+func NewMailbox(actorChan chan Envelope) *Mailbox {
+	m := &Mailbox{
+		actorChan: actorChan,
+		queue:     make([]Envelope, 0),
+	}
+	return m
+}
+
+func (m *Mailbox) Send(msg Envelope) {
+	select {
+	case m.actorChan <- msg:
+		// Successfully sent the message to the actorChan
+	default:
+		// The actorChan is full
+	}
+}
+
+func (m *Mailbox) Receive() Envelope {
+
+	return Envelope{}
+}
+
+func (m *Mailbox) Buffer(msg Envelope) {
+	m.queue = append(m.queue, msg)
+}
+
+func (m *Mailbox) Start() {
+
+}
+
+func (m *Mailbox) Stop() {
+
 }

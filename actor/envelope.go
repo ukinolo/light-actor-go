ce9440887ea00a70e595bd5c59edfd4bd68d4213
milossdjuric <milossdjuric@yahoo.com>
2024-06-16 21:52:29 +0200

    remove sender from Envelope

--- actor/envelope.go
+++ actor/envelope.go
@@ -3,24 +3,15 @@ package actor
 import "github.com/google/uuid"
 
 type Envelope struct {
-	sender   PID
 	message  interface{}
 	receiver PID
 }
 
-func NewEnvelope(sender PID, message interface{}, receiver PID) *Envelope {
+func NewEnvelope(message interface{}, receiver PID) *Envelope {
 	return &Envelope{
-		sender:   sender,
 		message:  message,
 		receiver: receiver,
 	}
-}
-
-func (e *Envelope) Sender() *PID {
-	if e.sender.ID == uuid.Nil {
-		return nil
-	}
-	return &e.sender
 }
 
 func (e *Envelope) Receiver() *PID {
@@ -30,15 +21,9 @@ func (e *Envelope) Receiver() *PID {
 	return &e.receiver
 }
 
-func (e *Envelope) Unwrap() (*PID, interface{}, *PID) {
-	var sender *PID
-	var receiver *PID
+func (e *Envelope) Unwrap() (interface{}, *PID) {
 
-	if e.sender.ID != uuid.Nil {
-		sender = &e.sender
-	} else {
-		sender = nil
-	}
+	var receiver *PID
 
 	if e.receiver.ID == uuid.Nil {
 		receiver = &e.receiver
@@ -46,5 +31,5 @@ func (e *Envelope) Unwrap() (*PID, interface{}, *PID) {
 		receiver = nil
 	}
 
-	return sender, e.message, receiver
+	return e.message, receiver
 }

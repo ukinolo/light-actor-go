8ff151e2627edfb0421f810f8b7f25fdfd9afc0f
ukinolo <ykithethird@gmail.com>
2024-06-23 14:21:32 +0200

    removing comments

--- actor/registry.go
+++ actor/registry.go
@@ -6,22 +6,22 @@ import (
 
 type Registry struct {
 	mapping map[PID]chan Envelope // Stores mailbox channels
-	mu      sync.RWMutex          // Mutex to make the registry thread-safe
+	mu      sync.RWMutex
 }
 
 func NewRegistry() *Registry {
-	return &Registry{mapping: make(map[PID]chan Envelope), mu: sync.RWMutex{}}
+	return &Registry{mapping: make(map[PID]chan Envelope)}
 }
 
 func (r *Registry) Add(pid PID, ch chan Envelope) error {
-	r.mu.Lock()         // Lock the mutex for writing
-	defer r.mu.Unlock() // Ensure the mutex is unlocked after the operation
+	r.mu.Lock()
+	defer r.mu.Unlock()
 	r.mapping[pid] = ch
 	return nil
 }
 
 func (r *Registry) Find(pid PID) chan Envelope {
-	r.mu.RLock()         // Lock the mutex for reading
-	defer r.mu.RUnlock() // Ensure the mutex is unlocked after the operation
+	r.mu.RLock()
+	defer r.mu.RUnlock()
 	return r.mapping[pid]
 }
